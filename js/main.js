9fa2d198417b75bd36f35e87f2a0209c465373d6
MacBook Air <macbookair@MacBook-Air.local>
2023-07-10 16:19:43 +0700

    3rd commit

--- js/main.js
+++ js/main.js
@@ -251,6 +251,27 @@ $(".schedule-carousel").owlCarousel({
   },
 });
 
+// Gallery Slider
+$(".facility-carousel").owlCarousel({
+  items: 2,
+  loop: true,
+  margin: 0,
+  dots: false,
+  nav: true,
+  navText: [
+    '<i class="fa fa-angle-left"></i>',
+    '<i class="fa fa-angle-right"></i>',
+  ],
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    992: {
+      items: 2,
+    },
+  },
+});
 /* ----------------------------
     Service Image Changes
 ------------------------------- */
